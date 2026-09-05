2dadf1b78f5ba525e9b978f27ede57ef60b8abe3
Alexander Widman <119602587+AlexWidman@users.noreply.github.com>
2026-09-05 17:19:42 +0200

    Update ListeningEvent to prepare for parser for data

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,11 +1,46 @@
+// Extended listening data from Spotify
+export type SpotifyStreamingRecord = {
+  ts: string
+  platform: string
+  ms_played: number
+  conn_country: string
+
+  master_metadata_track_name: string | null
+  master_metadata_album_artist_name: string | null
+  master_metadata_album_album_name: string | null
+
+  spotify_track_uri: string | null
+
+  episode_name: string | null
+  episode_show_name: string | null
+  spotify_episode_uri: string | null
+
+  audiobook_title: string | null
+  audiobook_uri: string | null
+  audiobook_chapter_uri: string | null
+  audiobook_chapter_title: string | null
+
+  reason_start: string
+  reason_end: string
+
+  shuffle: boolean
+  skipped: boolean
+  offline: boolean
+  offline_timestamp: number | null
+  incognito_mode: boolean
+}
+
+// Data relevant for EchoLocus, picked out from the extended listening data
 export type ListeningEvent = {
   timestamp: string
   platform: string
   country: string
+
   trackName: string
   artistName: string
   albumName: string
   trackUri: string
+
   msPlayed: number
   skipped: boolean
 }
